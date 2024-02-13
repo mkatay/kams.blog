99cb6f6f0362cae23095442451811f087994b0c3
KMagdi <46417302+kmagdi@users.noreply.github.com>
2024-02-13 09:42:54 +0100

    first

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Example } from "./components/Example"
+import { Notfound } from "./pages/NotFound"
+
+
+function App() {
+  
+  return (
+    <>
+    {/*} <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  <Example/>*/}
+  <Notfound/>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import img404 from '../assets/errorRectangle.svg'
+import { MyButton } from "../components/MyButton";
+
+export const Notfound = () => {
+  return (
+    <div className="md:pl-[70px] relative bg-black  h-screen w-full md:flex">
+      <div className=" h-screen bg-black  flex flex-col justify-center items-center md:items-start lg:w-full px-2.5 md:px-0">
+        <h3 className="text-[#F3F3F3] text-8xl md:text-[200px] font-[800px] font-poppins">
+          404
+        </h3>
+        <h3 className="text-white text-3xl xl:text-[57px] font-semibold font-poppins whitespace-nowrap">
+          Page Not Found
+        </h3>
+        <h3 className="text-[#F3F3F3] text-2xl  xl:text-[32px] font-[500px] font-poppins xl:w-[651px] mt-10 mb-[152px] text-center md:text-start">
+          Sorry, we couldn’t find the page you ‘re looking for
+        </h3>
+        <div className="flex items-center bg-black ">
+         <MyButton text='Back to Home'/>
+        </div>
+      </div>
+      <div className="relative right-0 h-screen hidden md:flex ">
+        <img
+          alt="Tailwind Icon"
+          className="h-screen absolutec object-cover"
+          src={img404}
+        />
+      </div>
+    </div>
+  );
+};
+
